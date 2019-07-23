db8c2e76e5ab05ec1159f35815ada344ba9a1332
Ajay Ramachandran <dev@ajay.app>
2019-07-23 18:31:42 -0400

    Updated posting to work with extra database column.

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.get('/api/postVideoSponsorTimes', function (req, res) {
                         
                         if (row == null) {
                             //not a duplicate, execute query
-                            db.prepare("INSERT INTO sponsorTimes VALUES(?, ?, ?, ?, ?, ?, ?, ?)").run(videoID, startTime, endTime, 0, UUID, userID, hashedIP, timeSubmitted);
+                            db.prepare("INSERT INTO sponsorTimes VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)").run(videoID, startTime, endTime, 0, UUID, userID, hashedIP, timeSubmitted, 0);
 
                             res.sendStatus(200);
                         } else {
