393c131104911f83ebdfcf63fd511adb003135fe
Joe-Dowd <joedowd96@gmail.com>
2020-04-01 23:42:58 +0100

    remove duplicate videoID in test cases

--- test/cases/getSavedTimeForUser.js
+++ test/cases/getSavedTimeForUser.js
@@ -5,14 +5,13 @@ var getHash = require('../../src/utils/getHash.js');
 
 describe('getSavedTimeForUser', () => {
   before(() => {
-    db.exec("INSERT INTO sponsorTimes VALUES ('testtesttest', 1, 11, 2, 'abc1239999', '"+getHash("testman")+"', 0, 50, 0)");
+    db.exec("INSERT INTO sponsorTimes VALUES ('getSavedTimeForUser', 1, 11, 2, 'abc1239999', '"+getHash("testman")+"', 0, 50, 0)");
   }); 
 
   it('Should be able to get a 200', (done) => {
     request.get(utils.getbaseURL() 
      + "/api/getSavedTimeForUser?userID=testman", null, 
       (err, res, body) => {
-        console.log(res.statusCode);
         if (err) done(false);
         else if (res.statusCode !== 200) done("non 200");
         else done();
