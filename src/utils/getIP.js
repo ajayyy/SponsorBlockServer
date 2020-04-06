f1a7524acf3f283bbb4844bf91ca91720e22b1e8
Ajay Ramachandran <dev@ajay.app>
2020-04-06 17:29:52 -0400

    Fixed getIP module import

--- src/utils/getIP.js
+++ src/utils/getIP.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var config = require('./config.js');
+var config = require('../config.js');
 
 module.exports = function getIP(req) {
   return config.behindProxy ? req.headers['x-forwarded-for'] : req.connection.remoteAddress;
