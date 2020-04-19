950b8ecb55412ed4dd8af3c0fb2ba1b1cd1c0832
Ajay Ramachandran <dev@ajay.app>
2020-04-19 19:18:24 -0400

    Update getDaysSavedFormatted.js

--- src/routes/getDaysSavedFormatted.js
+++ src/routes/getDaysSavedFormatted.js
@@ -1,4 +1,4 @@
-var db = require('../databases/databases.js');
+var db = require('../databases/databases.js').db;
 
 module.exports = function getDaysSavedFormatted (req, res) {
   let row = db.prepare("select sum((endtime - starttime) / 60 / 60 / 24 * views) as dayssaved from sponsortimes where shadowhidden != 1").get();
