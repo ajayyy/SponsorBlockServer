cadf2640febbf3b3adc8ce28af02857a8cae89bf
Ajay Ramachandran <dev@ajay.app>
2020-07-27 19:54:38 -0400

    Add spacing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var config = require('./src/config.js');
 var createServer = require('./src/app.js');
 var server = createServer(() => {
-  console.log("Server started on port "+config.port+".");
+  console.log("Server started on port " + config.port + ".");
 });
