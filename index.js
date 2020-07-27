18f5d656de8610eeb25c64c989e29b28bb548113
Joe Dowd <joedowd96@gmail.com>
2020-07-27 05:51:49 +0100

    Aded option to proxy submisissions to another server to persist.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var config = require('./src/config.js');
 var createServer = require('./src/app.js');
 var server = createServer(() => {
-  console.log("Server started.");
+  console.log("Server started on port "+config.port+".");
 });
