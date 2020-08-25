2f2273b8a8bf0c875a9b910d1e5a436872c01362
Ajay Ramachandran <dev@ajay.app>
2020-08-24 20:12:57 -0400

    Added decimals to formatted time

--- src/utils/getFormattedTime.js
+++ src/utils/getFormattedTime.js
@@ -1,8 +1,9 @@
 //converts time in seconds to minutes:seconds
 module.exports = function getFormattedTime(seconds) {
   let minutes = Math.floor(seconds / 60);
-  let secondsDisplay = Math.round(seconds - minutes * 60);
-  if (secondsDisplay < 10) {
+  let seconds = seconds - minutes * 60;
+  let secondsDisplay = seconds.toFixed(3);
+  if (seconds < 10) {
       //add a zero
       secondsDisplay = "0" + secondsDisplay;
   }
