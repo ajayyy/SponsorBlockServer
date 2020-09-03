a053d87bd2d43d2c45504375217e7505ecd24b42
Ajay Ramachandran <dev@ajay.app>
2020-09-03 12:04:49 -0400

    Catch user counter errors

--- src/middleware/userCounter.js
+++ src/middleware/userCounter.js
@@ -1,11 +1,16 @@
 var request = require('request');
 
-var config = require('../config.js');
-var getIP = require('../utils/getIP.js');
+const config = require('../config.js');
+const getIP = require('../utils/getIP.js');
 const getHash = require('../utils/getHash.js');
+const logger = require('../utils/logger.js');
 
 module.exports = function userCounter(req, res, next) {
-    request.post(config.userCounterURL + "/api/v1/addIP?hashedIP=" + getHash(getIP(req), 1));
+    try {
+        request.post(config.userCounterURL + "/api/v1/addIP?hashedIP=" + getHash(getIP(req), 1));
+    } catch(e) {
+        logger.debug("Failing to connect to user counter at: " + config.userCounterURL);
+    }
 
     next();
 }
