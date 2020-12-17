c8d5cec3383bca856e74329e9cb8f259d84f65de
Ajay Ramachandran <dev@ajay.app>
2020-12-17 00:25:33 -0500

    Fix redis type issues

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -2,18 +2,15 @@ import {config} from '../config';
 import {Logger} from './logger';
 import redis, {Callback} from 'redis';
 
-let get, set;
+let exportObject = {
+    get: (key: string, callback?: Callback<string | null>) => callback(null, undefined),
+    set: (key: string, value: string, callback?: Callback<string | null>) => callback(null, undefined)
+};
+
 if (config.redis) {
     Logger.info('Connected to redis');
     const client = redis.createClient(config.redis);
-    get = client.get;
-    set = client.set;
-} else {
-    get = (key: string, callback?: Callback<string | null>) => callback(null, undefined);
-    set = (key: string, value: string, callback?: Callback<string | null>) => callback(null, undefined);
+    exportObject = client;
 }
 
-export {
-    get,
-    set,
-};
+export default exportObject;
