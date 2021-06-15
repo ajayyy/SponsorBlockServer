062faba8d1cf366fac15781d85e49d11b5798136
Michael C <michael@mchang.name>
2021-06-14 20:29:02 -0400

    remove CORS from nginx, add to express

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,6 +2,7 @@ import {NextFunction, Request, Response} from 'express';
 
 export function corsMiddleware(req: Request, res: Response, next: NextFunction) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE")
     next();
 }
