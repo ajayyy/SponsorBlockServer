214ddc980786c6dc583eb15c8178ed0d2b96aecd
Haidang666 <hdcompany123@gmail.com>
2021-06-29 14:56:57 +0700

    Add warning reason in postWarning

---
+++ databases/_upgrade_sponsorTimes_17.sql
@@ -0,0 +1,19 @@
+BEGIN TRANSACTION;
+
+/* Add reason field */
+CREATE TABLE "sqlb_temp_table_17" (
+	"userID" TEXT NOT NULL,
+	"issueTime" INTEGER NOT NULL,
+	"issuerUserID" TEXT NOT NULL,
+	enabled INTEGER NOT NULL,
+	"reason" TEXT NOT NULL default ''
+);
+
+INSERT INTO sqlb_temp_table_17 SELECT "userID","issueTime","issuerUserID","enabled", "" FROM "warnings";
+
+DROP TABLE warnings;
+ALTER TABLE sqlb_temp_table_17 RENAME TO "warnings";;
+
+UPDATE "config" SET value = 17 WHERE key = 'version';
+
+COMMIT;
