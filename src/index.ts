6b5156468ca1ca60f97396f2e79a5cfbd9ebc9c4
Haidang666 <hdcompany123@gmail.com>
2021-07-09 11:46:04 +0700

    Add archive downvote segment cron

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import {config} from "./config";
 import {initDb} from './databases/databases';
 import {createServer} from "./app";
 import {Logger} from "./utils/logger";
+import {startAllCrons} from "./cronjob";
 
 async function init() {
     await initDb();
 
     createServer(() => {
         Logger.info("Server started on port " + config.port + ".");
+
+        // ignite cron job after server created
+        startAllCrons();
     });
 }
- 
+
 init();
