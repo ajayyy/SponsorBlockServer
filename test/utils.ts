b39c06a9efb0631950cb63cf6a01b8044fa045e6
Michael C <michael@mchang.name>
2021-08-02 02:47:49 -0400

    add tests, new partialDeepEquals

--- test/utils.ts
+++ test/utils.ts
@@ -8,3 +8,19 @@ export function getbaseURL(): string {
  * Duplicated from Mocha types. TypeScript doesn't infer that type by itself for some reason.
  */
 export type Done = (err?: any) => void;
+
+/**
+ *
+ * Check object contains expected properties
+ */
+export const partialDeepEquals = (actual: Record<string, any>, expected: Record<string, any>): boolean => {
+    // loop over key, value of expected
+    for (const [ key, value ] of Object.entries(expected)) {
+        // if value is object or array, recurse
+        if (Array.isArray(value) || typeof value === "object") {
+            if (!partialDeepEquals(actual?.[key], value)) return false;
+        }
+        else if (actual?.[key] !== value) return false;
+    }
+    return true;
+};
