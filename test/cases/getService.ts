93c69248d9ddd08f7b9656e752953ede6db270ad
Haidang666 <hdcompany123@gmail.com>
2021-09-13 14:49:17 +0700

    Add getService helper function

---
+++ test/cases/getService.ts
@@ -0,0 +1,29 @@
+import { getService } from "../../src/utils/getService";
+import { Service } from "../../src/types/segments.model";
+
+import assert from "assert";
+
+describe("getService", () => {
+    it("Should return youtube if not match", () => {
+        assert.strictEqual(getService(), Service.YouTube);
+        assert.strictEqual(getService(""), Service.YouTube);
+        assert.strictEqual(getService("test", "not exist"), Service.YouTube);
+        assert.strictEqual(getService(null, null), Service.YouTube);
+        assert.strictEqual(getService(undefined, undefined), Service.YouTube);
+        assert.strictEqual(getService(undefined), Service.YouTube);
+    });
+
+    it("Should return Youtube", () => {
+        assert.strictEqual(getService("youtube"), Service.YouTube);
+        assert.strictEqual(getService("   Youtube   "), Service.YouTube);
+        assert.strictEqual(getService("   YouTube   "), Service.YouTube);
+        assert.strictEqual(getService(undefined, "   YouTube   "), Service.YouTube);
+    });
+
+    it("Should return PeerTube", () => {
+        assert.strictEqual(getService("PeerTube"), Service.PeerTube);
+        assert.strictEqual(getService("   PeerTube   "), Service.PeerTube);
+        assert.strictEqual(getService("   peertube   "), Service.PeerTube);
+        assert.strictEqual(getService(undefined, "   PeerTube   "), Service.PeerTube);
+    });
+});
