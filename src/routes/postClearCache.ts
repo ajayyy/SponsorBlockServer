03c95ca15867fca402100836a607ebe4af18f2c8
Michael C <michael@mchang.name>
2021-11-15 01:48:14 -0500

    rename clearSegmentCache

--- src/routes/postClearCache.ts
+++ src/routes/postClearCache.ts
@@ -39,7 +39,7 @@ export async function postClearCache(req: Request, res: Response): Promise<Respo
     }
 
     try {
-        QueryCacher.clearVideoCache({
+        QueryCacher.clearSegmentCache({
             videoID,
             hashedVideoID,
             service
