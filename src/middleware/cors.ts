164a9dab17b3aa71fec064ba0d22b12c6fd09b98
Michael C <michael@mchang.name>
2022-01-06 18:42:48 -0500

    refactored nginx configs
    - handle CORS within nginx
    - remove allow-headers from CORS
    - add custom dynamic error pages
    - remove unused fastcgi
    - removed OPTIONS backend
    - remove previous custom errorDirective
    - moved proxy directives to appropiate file

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 
 export function corsMiddleware(req: Request, res: Response, next: NextFunction): void {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
     next();
 }
