77de17c81021b1bbf99bab8d4e066aca72bb8593
Michael C <michael@mchang.name>
2022-01-06 21:31:34 -0500

    add allow-headers content-type

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,5 +3,6 @@ import { NextFunction, Request, Response } from "express";
 export function corsMiddleware(req: Request, res: Response, next: NextFunction): void {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 }
