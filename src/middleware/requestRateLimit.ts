41c92da37e46eeca3a4ca5e1b2a58e4789103d65
Ajay <dev@ajay.app>
2022-04-13 13:50:30 -0400

    Upgrade express-rate-limit

--- src/middleware/requestRateLimit.ts
+++ src/middleware/requestRateLimit.ts
@@ -1,19 +1,20 @@
 import { getIP } from "../utils/getIP";
 import { getHash } from "../utils/getHash";
 import { getHashCache } from "../utils/getHashCache";
-import rateLimit from "express-rate-limit";
+import rateLimit, { RateLimitRequestHandler } from "express-rate-limit";
 import { RateLimitConfig } from "../types/config.model";
 import { Request } from "express";
 import { isUserVIP } from "../utils/isUserVIP";
 import { UserID } from "../types/user.model";
 
-export function rateLimitMiddleware(limitConfig: RateLimitConfig, getUserID?: (req: Request) => UserID): rateLimit.RateLimit {
+export function rateLimitMiddleware(limitConfig: RateLimitConfig, getUserID?: (req: Request) => UserID): RateLimitRequestHandler {
     return rateLimit({
         windowMs: limitConfig.windowMs,
         max: limitConfig.max,
         message: limitConfig.message,
         statusCode: limitConfig.statusCode,
-        headers: false,
+        legacyHeaders: false,
+        standardHeaders: false,
         keyGenerator: (req) => {
             return getHash(getIP(req), 1);
         },
