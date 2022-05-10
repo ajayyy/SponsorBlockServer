78acb4a76a66b34b266f60368aa94e734ce9fa2e
Ajay <dev@ajay.app>
2022-05-10 14:27:43 -0400

    am wrong again

--- databases/_upgrade_private_8.sql
+++ databases/_upgrade_private_8.sql
@@ -7,7 +7,7 @@ ALTER TABLE "categoryVotes" ADD "id" SERIAL PRIMARY KEY;
 ALTER TABLE "sponsorTimes" ADD "id" SERIAL PRIMARY KEY;
 ALTER TABLE "config" ADD PRIMARY KEY ("key");
 ALTER TABLE "ratings" ADD "id" SERIAL PRIMARY KEY;
-ALTER TABLE "tempVipLog" ADD PRIMARY KEY ("issuerUserID", "targetUserID");
+ALTER TABLE "tempVipLog" ADD "id" SERIAL PRIMARY KEY;
 ALTER TABLE "votes" ADD PRIMARY KEY ("UUID", "userID");
 
 UPDATE "config" SET value = 8 WHERE key = 'version';
