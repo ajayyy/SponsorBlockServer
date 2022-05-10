34771e96fea04890bff6aea13eb1a78992ce5f3d
Ajay <dev@ajay.app>
2022-05-10 14:20:07 -0400

    change primary key for category votes

--- databases/_upgrade_private_8.sql
+++ databases/_upgrade_private_8.sql
@@ -3,7 +3,7 @@ BEGIN TRANSACTION;
 -- Add primary keys
 
 ALTER TABLE "userNameLogs" ADD PRIMARY KEY ("userID");
-ALTER TABLE "categoryVotes" ADD PRIMARY KEY ("UUID", "userID", "category");
+ALTER TABLE "categoryVotes" ADD "id" SERIAL PRIMARY KEY;
 ALTER TABLE "sponsorTimes" ADD "id" SERIAL PRIMARY KEY;
 ALTER TABLE "config" ADD PRIMARY KEY ("key");
 ALTER TABLE "ratings" ADD "id" SERIAL PRIMARY KEY;
