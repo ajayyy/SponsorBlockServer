21f7d5d9384b57bf38189d05de5004b3684b43d0
Ajay <dev@ajay.app>
2022-05-17 12:53:48 -0400

    Don't add primary keys with sqlite

--- databases/_upgrade_private_8.sql
+++ databases/_upgrade_private_8.sql
@@ -2,13 +2,13 @@ BEGIN TRANSACTION;
 
 -- Add primary keys
 
-ALTER TABLE "userNameLogs" ADD "id" SERIAL PRIMARY KEY;
-ALTER TABLE "categoryVotes" ADD "id" SERIAL PRIMARY KEY;
-ALTER TABLE "sponsorTimes" ADD "id" SERIAL PRIMARY KEY;
-ALTER TABLE "config" ADD PRIMARY KEY ("key");
-ALTER TABLE "ratings" ADD "id" SERIAL PRIMARY KEY;
-ALTER TABLE "tempVipLog" ADD "id" SERIAL PRIMARY KEY;
-ALTER TABLE "votes" ADD "id" SERIAL PRIMARY KEY;
+ALTER TABLE "userNameLogs" ADD "id" SERIAL PRIMARY KEY; --!sqlite-ignore
+ALTER TABLE "categoryVotes" ADD "id" SERIAL PRIMARY KEY; --!sqlite-ignore
+ALTER TABLE "sponsorTimes" ADD "id" SERIAL PRIMARY KEY; --!sqlite-ignore
+ALTER TABLE "config" ADD PRIMARY KEY ("key"); --!sqlite-ignore
+ALTER TABLE "ratings" ADD "id" SERIAL PRIMARY KEY; --!sqlite-ignore
+ALTER TABLE "tempVipLog" ADD "id" SERIAL PRIMARY KEY; --!sqlite-ignore
+ALTER TABLE "votes" ADD "id" SERIAL PRIMARY KEY; --!sqlite-ignore
 
 UPDATE "config" SET value = 8 WHERE key = 'version';
 
--- databases/_upgrade_sponsorTimes_32.sql
+++ databases/_upgrade_sponsorTimes_32.sql
@@ -2,17 +2,17 @@ BEGIN TRANSACTION;
 
 -- Add primary keys
 
-ALTER TABLE "sponsorTimes" ADD PRIMARY KEY ("UUID");
-ALTER TABLE "vipUsers" ADD PRIMARY KEY ("userID");
-ALTER TABLE "userNames" ADD PRIMARY KEY ("userID");
-ALTER TABLE "categoryVotes" ADD "id" SERIAL PRIMARY KEY;
-ALTER TABLE "lockCategories" ADD "id" SERIAL PRIMARY KEY;
-ALTER TABLE "warnings" ADD PRIMARY KEY ("userID", "issueTime");
-ALTER TABLE "shadowBannedUsers" ADD PRIMARY KEY ("userID");
-ALTER TABLE "unlistedVideos" ADD "id" SERIAL PRIMARY KEY;
-ALTER TABLE "config" ADD PRIMARY KEY ("key");
-ALTER TABLE "archivedSponsorTimes" ADD PRIMARY KEY ("UUID");
-ALTER TABLE "ratings" ADD "id" SERIAL PRIMARY KEY;
+ALTER TABLE "sponsorTimes" ADD PRIMARY KEY ("UUID"); --!sqlite-ignore
+ALTER TABLE "vipUsers" ADD PRIMARY KEY ("userID"); --!sqlite-ignore
+ALTER TABLE "userNames" ADD PRIMARY KEY ("userID"); --!sqlite-ignore
+ALTER TABLE "categoryVotes" ADD "id" SERIAL PRIMARY KEY; --!sqlite-ignore
+ALTER TABLE "lockCategories" ADD "id" SERIAL PRIMARY KEY; --!sqlite-ignore
+ALTER TABLE "warnings" ADD PRIMARY KEY ("userID", "issueTime"); --!sqlite-ignore
+ALTER TABLE "shadowBannedUsers" ADD PRIMARY KEY ("userID"); --!sqlite-ignore
+ALTER TABLE "unlistedVideos" ADD "id" SERIAL PRIMARY KEY; --!sqlite-ignore
+ALTER TABLE "config" ADD PRIMARY KEY ("key"); --!sqlite-ignore
+ALTER TABLE "archivedSponsorTimes" ADD PRIMARY KEY ("UUID"); --!sqlite-ignore
+ALTER TABLE "ratings" ADD "id" SERIAL PRIMARY KEY; --!sqlite-ignore
 
 UPDATE "config" SET value = 32 WHERE key = 'version';
 
