dd47f876164dee0cd073c7d0f7061f5ae0732442
Ajay <dev@ajay.app>
2022-07-20 01:21:13 -0400

    Add more things to read from replica

--- src/utils/isUserVIP.ts
+++ src/utils/isUserVIP.ts
@@ -2,5 +2,6 @@ import { db } from "../databases/databases";
 import { HashedUserID } from "../types/user.model";
 
 export async function isUserVIP(userID: HashedUserID): Promise<boolean> {
-    return (await db.prepare("get", `SELECT count(*) as "userCount" FROM "vipUsers" WHERE "userID" = ? LIMIT 1`, [userID]))?.userCount > 0;
+    return (await db.prepare("get", `SELECT count(*) as "userCount" FROM "vipUsers" WHERE "userID" = ? LIMIT 1`, 
+        [userID], { useReplica: true }))?.userCount > 0;
 }
