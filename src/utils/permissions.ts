cb1cc10278fc687d3019565c81014a6addb939f5
Ajay <dev@ajay.app>
2022-07-31 21:05:39 -0400

    Extend restriction to all categories

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -21,14 +21,14 @@ export async function canSubmit(userID: HashedUserID, category: Category): Promi
                     hasFeature(userID, Feature.ChapterSubmitter)
                 ])
             };
-        case "filler":
+        default:
             return {
                 canSubmit: await oneOf([isUserVIP(userID),
                     (async () => (await getReputation(userID)) > config.minReputationToSubmitFiller)(),
                     hasFeature(userID, Feature.FillerSubmitter),
                     (async () => (await db.prepare("get", `SELECT count(*) as "submissionCount" FROM "sponsorTimes" WHERE "userID" = ? AND category != 'filler' AND "timeSubmitted" < 1654010691000 LIMIT 4`, [userID], { useReplica: true }))?.submissionCount > 3)()
                 ]),
-                reason: "Someone has submitted over 1.9 million spam filler submissions and refuses to stop even after talking with them, so we have to restrict it for now. You can request submission access on chat.sponsor.ajay.app/#filler, discord.gg/SponsorBlock or matrix.to/#/#sponsor:ajay.app"
+                reason: "Unfortunately, someone is doing a targetd attack and as a temporary emergency measure, segment submission for new users is disabled. You can request submission access on chat.sponsor.ajay.app/#filler, discord.gg/SponsorBlock or matrix.to/#/#sponsor:ajay.app"
             };
     }
 
