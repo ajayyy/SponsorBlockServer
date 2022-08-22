44d541bafeb188be9d441fcf594489b98ba70af0
Ajay <dev@ajay.app>
2022-08-22 11:59:08 -0400

    Don't use replica for is vip

--- src/utils/isUserVIP.ts
+++ src/utils/isUserVIP.ts
@@ -3,5 +3,5 @@ import { HashedUserID } from "../types/user.model";
 
 export async function isUserVIP(userID: HashedUserID): Promise<boolean> {
     return (await db.prepare("get", `SELECT count(*) as "userCount" FROM "vipUsers" WHERE "userID" = ? LIMIT 1`,
-        [userID], { useReplica: true }))?.userCount > 0;
+        [userID]))?.userCount > 0;
 }
