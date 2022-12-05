89e122210e0255f1a6b99600c2164cb6c979655b
Ajay <dev@ajay.app>
2022-12-04 19:54:44 -0500

    Remove extra hash

--- src/middleware/userCounter.ts
+++ src/middleware/userCounter.ts
@@ -2,12 +2,11 @@ import axios from "axios";
 import { Logger } from "../utils/logger";
 import { config } from "../config";
 import { getIP } from "../utils/getIP";
-import { getHash } from "../utils/getHash";
 import { NextFunction, Request, Response } from "express";
 
 export function userCounter(req: Request, res: Response, next: NextFunction): void {
     if (req.method !== "OPTIONS") {
-        axios.post(`${config.userCounterURL}/api/v1/addIP?hashedIP=${getHash(getIP(req), 1)}`)
+        axios.post(`${config.userCounterURL}/api/v1/addIP?hashedIP=${getIP(req)}`)
             .catch(() => Logger.debug(`Failing to connect to user counter at: ${config.userCounterURL}`));
     }
 
