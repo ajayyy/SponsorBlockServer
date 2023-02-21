e6f54f11f02a0410a3d0e2c1efb713c3affb7556
Michael C <michael@mchang.name>
2023-02-21 17:00:23 -0500

    decompose postSkipSegments more

---
+++ test/cases/postSkipSegmentsLocked.ts
@@ -0,0 +1,70 @@
+import assert from "assert";
+import { postSkipSegmentJSON } from "./postSkipSegments";
+import { getHash } from "../../src/utils/getHash";
+import { db } from "../../src/databases/databases";
+
+describe("postSkipSegments - LockedVideos", () => {
+    const userIDOne = "postSkip-DurationUserOne";
+    const VIPLockUser = "VIPUser-lockCategories";
+    const videoID = "lockedVideo";
+    const userID = userIDOne;
+
+    before(() => {
+        const insertLockCategoriesQuery = `INSERT INTO "lockCategories" ("userID", "videoID", "category", "reason") VALUES(?, ?, ?, ?)`;
+        db.prepare("run", insertLockCategoriesQuery, [getHash(VIPLockUser), videoID, "sponsor", "Custom Reason"]);
+        db.prepare("run", insertLockCategoriesQuery, [getHash(VIPLockUser), videoID, "intro", ""]);
+    });
+
+    it("Should return 403 and custom reason for submiting in lockedCategory", (done) => {
+        postSkipSegmentJSON({
+            userID,
+            videoID,
+            segments: [{
+                segment: [1, 10],
+                category: "sponsor",
+            }],
+        })
+            .then(res => {
+                assert.strictEqual(res.status, 403);
+                assert.match(res.data, /Reason: /);
+                assert.match(res.data, /Custom Reason/);
+                done();
+            })
+            .catch(err => done(err));
+    });
+
+    it("Should return not be 403 when submitting with locked category but unlocked actionType", (done) => {
+        postSkipSegmentJSON({
+            userID,
+            videoID,
+            segments: [{
+                segment: [1, 10],
+                category: "sponsor",
+                actionType: "mute"
+            }],
+        })
+            .then(res => {
+                assert.strictEqual(res.status, 200);
+                done();
+            })
+            .catch(err => done(err));
+    });
+
+    it("Should return 403 for submiting in lockedCategory", (done) => {
+        postSkipSegmentJSON({
+            userID,
+            videoID,
+            segments: [{
+                segment: [1, 10],
+                category: "intro",
+            }],
+        })
+            .then(res => {
+                assert.strictEqual(res.status, 403);
+                assert.doesNotMatch(res.data, /Lock reason: /);
+                assert.doesNotMatch(res.data, /Custom Reason/);
+                done();
+            })
+            .catch(err => done(err));
+    });
+});
---
+++ test/cases/postSkipSegmentsShadowban.ts
@@ -0,0 +1,68 @@
+import assert from "assert";
+import { postSkipSegmentParam } from "./postSkipSegments";
+import { getHash } from "../../src/utils/getHash";
+import { db } from "../../src/databases/databases";
+import { ImportMock } from "ts-mock-imports";
+import * as YouTubeAPIModule from "../../src/utils/youtubeApi";
+import { YouTubeApiMock } from "../mocks/youtubeMock";
+
+const mockManager = ImportMock.mockStaticClass(YouTubeAPIModule, "YouTubeAPI");
+const sinonStub = mockManager.mock("listVideos");
+sinonStub.callsFake(YouTubeApiMock.listVideos);
+
+describe("postSkipSegments - shadowban", () => {
+    const banUser01 = "postSkip-banUser01";
+    const banUser01Hash = getHash(banUser01);
+
+    const shadowBanVideoID1 = "postSkipBan1";
+    const shadowBanVideoID2 = "postSkipBan2";
+
+    const queryDatabaseShadowhidden = (videoID: string) => db.prepare("get", `SELECT "startTime", "endTime", "shadowHidden", "userID" FROM "sponsorTimes" WHERE "videoID" = ?`, [videoID]);
+
+    before(() => {
+        db.prepare("run", `INSERT INTO "shadowBannedUsers" ("userID") VALUES(?)`, [banUser01Hash]);
+    });
+
+    it("Should automatically shadowban segments if user is banned", (done) => {
+        const videoID = shadowBanVideoID1;
+        postSkipSegmentParam({
+            videoID,
+            startTime: 0,
+            endTime: 10,
+            category: "sponsor",
+            userID: banUser01
+        })
+            .then(async res => {
+                assert.strictEqual(res.status, 200);
+                const row = await queryDatabaseShadowhidden(videoID);
+                const expected = {
+                    startTime: 0,
+                    endTime: 10,
+                    shadowHidden: 1,
+                    userID: banUser01Hash
+                };
+                assert.deepStrictEqual(row, expected);
+                done();
+            })
+            .catch(err => done(err));
+    });
+
+    it("Should not add full segments to database if user if shadowbanned", (done) => {
+        const videoID = shadowBanVideoID2;
+        postSkipSegmentParam({
+            videoID,
+            startTime: 0,
+            endTime: 0,
+            category: "sponsor",
+            actionType: "full",
+            userID: banUser01
+        })
+            .then(async res => {
+                assert.strictEqual(res.status, 200);
+                const row = await queryDatabaseShadowhidden(videoID);
+                assert.strictEqual(row, undefined);
+                done();
+            })
+            .catch(err => done(err));
+    });
+});
