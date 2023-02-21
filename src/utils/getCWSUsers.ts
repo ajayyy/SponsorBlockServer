c6795a783dbad21c573b4d5bd731c3e6f96c9d98
Michael C <michael@mchang.name>
2023-02-21 03:24:32 -0500

    fix getCWS util

--- src/utils/getCWSUsers.ts
+++ src/utils/getCWSUsers.ts
@@ -5,7 +5,7 @@ export const getCWSUsers = (extID: string) =>
     axios.post(`https://chrome.google.com/webstore/ajax/detail?pv=20210820&id=${extID}`)
         .then((res) => res.data.split("\n")[2])
         .then((data) => JSON.parse(data))
-        .then((data) => (data[1][1][0][23]).replaceAll(/,|\+/,""))
+        .then((data) => (data[1][1][0][23]).replaceAll(/,|\+/g,""))
         .then((data) => parseInt(data))
         .catch((err) => {
             Logger.error(`Error getting chrome users - ${err}`);
