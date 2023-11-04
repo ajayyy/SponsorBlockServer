88a74352a0bcaa674da619cc086feead9a0474b1
Michael C <michael@mchang.name>
2023-11-04 17:56:06 -0400

    add more small tests
    - getUsername
    - postSkipSegments400Stub
    - downvoteSegmentArchive

--- test/cases/getUsername.ts
+++ test/cases/getUsername.ts
@@ -1,4 +1,5 @@
-import { getHash } from "../../src/utils/getHash";
+import { genRandomValue } from "../utils/genRandom";
+import { genAnonUser } from "../utils/genUser";
 import { client } from "../utils/httpClient";
 import assert from "assert";
 
@@ -17,37 +18,24 @@ const postSetUserName = (userID: string, username: string) => client({
     }
 });
 
-const userOnePrivate = "getUsername_0";
-const userOnePublic = getHash(userOnePrivate);
-const userOneUsername = "getUsername_username";
-
 describe("getUsername test", function() {
-    it("Should get back publicUserID if not set", (done) => {
-        getUsername(userOnePrivate)
-            .then(result => {
-                assert.strictEqual(result.data.userName, userOnePublic);
-                done();
-            })
-            .catch(err => done(err));
+    it("Should get back publicUserID if not set", async () => {
+        const user = genAnonUser();
+        const result = await getUsername(user.privID);
+        return assert.strictEqual(result.data.userName, user.pubID);
     });
-    it("Should be able to get username after setting", (done) => {
-        postSetUserName(userOnePrivate, userOneUsername)
+    it("Should be able to get username after setting", () => {
+        const user = genAnonUser();
+        const username = genRandomValue("username", "getUsername");
+        return postSetUserName(user.privID, username)
             .then(async () => {
-                const result = await getUsername(userOnePrivate);
+                const result = await getUsername(user.privID);
                 const actual = result.data.userName;
-                assert.strictEqual(actual, userOneUsername);
-                done();
-            })
-            .catch(err => done(err));
+                assert.strictEqual(actual, username);
+            });
     });
-    it("Should return 400 if no userID provided", (done) => {
-        client({
-            url: "/api/getUsername"
-        })
-            .then(res => {
-                assert.strictEqual(res.status, 400);
-                done();
-            })
-            .catch(err => done(err));
+    it("Should return 400 if no userID provided", () => {
+        client({ url: "/api/getUsername" })
+            .then(res => assert.strictEqual(res.status, 400));
     });
 });
