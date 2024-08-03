6ae86dda034f82c65c0848689d0614380740a9a5
Tristan <tristan@tnet.moe>
2024-08-03 10:11:42 +0200

    feat(userNames): impl createdAt and updatedAt

--- databases/_upgrade_sponsorTimes_41.sql
+++ databases/_upgrade_sponsorTimes_41.sql
@@ -6,4 +6,8 @@ ALTER TABLE "vipUsers" ADD "createdAt" INTEGER NOT NULL;
 -- sponsorTimes
 -- Don't create createdAt -> timeSubmitted field
 ALTER TABLE "vipUsers" ADD "sponsorTimes" INTEGER NOT NULL;
+
+-- userNames
+ALTER TABLE "userNames" ADD "createdAt" INTEGER NOT NULL;
+ALTER TABLE "userNames" ADD "updatedAt" INTEGER NOT NULL;
 COMMIT;
