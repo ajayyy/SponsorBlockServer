d28ac39d4f8c249b4438004dfd4d7fd4a93ea27c
Ajay <dev@ajay.app>
2024-09-07 23:31:16 -0400

    Allow newly used header

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,6 +3,6 @@ import { NextFunction, Request, Response } from "express";
 export function corsMiddleware(req: Request, res: Response, next: NextFunction): void {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, If-None-Match");
+    res.header("Access-Control-Allow-Headers", "Content-Type, If-None-Match, x-client-name");
     next();
 }
