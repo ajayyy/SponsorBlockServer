fb3abb32168638b2b803d1ae6c84d074e3a8b4c2
Ajay <dev@ajay.app>
2025-02-06 03:01:22 -0500

    Fix index for casual votes

--- databases/_sponsorTimes_indexes.sql
+++ databases/_sponsorTimes_indexes.sql
@@ -184,7 +184,7 @@ CREATE INDEX IF NOT EXISTS "casualVotes_timeSubmitted"
 
 CREATE INDEX IF NOT EXISTS "casualVotes_userID_timeSubmitted"
     ON public."casualVotes" USING btree
-    ("videoID" COLLATE pg_catalog."default" ASC NULLS LAST, "service" COLLATE pg_catalog."default" ASC NULLS LAST, "userID" COLLATE pg_catalog."default" DESC NULLS LAST, "timeSubmitted" DESC NULLS LAST)
+    ("videoID" COLLATE pg_catalog."default" ASC NULLS LAST, "service" COLLATE pg_catalog."default" ASC NULLS LAST, "timeSubmitted" DESC NULLS LAST)
     TABLESPACE pg_default;
 
 CREATE INDEX IF NOT EXISTS "casualVotes_videoID"
