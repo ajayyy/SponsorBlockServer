82af8f200b20ea40220d1c6b0a8f13bc11df5f17
Ajay <dev@ajay.app>
2025-04-08 14:21:24 -0400

    Add better ua parsing

--- src/utils/userAgent.ts
+++ src/utils/userAgent.ts
@@ -5,6 +5,11 @@ export function parseUserAgent(userAgent: string): string {
         return `Vanced/${ua.match(/.android.youtube\/([^\s]+)/)[1]}`;
     }
 
+    const revanced = ua.match(/RVX\S+|ReVanced\S+/);
+    if (revanced) {
+        return revanced[0];
+    }
+
     if (ua.match(/(mpv_sponsorblock\/)|(^python-requests)|(^GuzzleHttp\/)|(^PostmanRuntime\/)/)) {
         return ua;
     }
