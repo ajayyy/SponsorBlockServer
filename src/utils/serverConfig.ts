f67244663e5717644ea04fdc998973b8b776ba20
Ajay <dev@ajay.app>
2025-04-08 15:56:23 -0400

    Use alias for getting server config

--- src/utils/serverConfig.ts
+++ src/utils/serverConfig.ts
@@ -1,7 +1,7 @@
 import { db } from "../databases/databases";
 
 export async function getServerConfig(key: string): Promise<string | null> {
-    const row = await db.prepare("get", `SELECT "value" FROM "config" WHERE "key" = ?`, [key]);
+    const row = await db.prepare("get", `SELECT "value" as v FROM "config" WHERE "key" = ?`, [key]);
 
-    return row?.value ?? null;
+    return row?.v ?? null;
 }
