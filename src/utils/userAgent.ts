39f8dc6c22227f55533af507356d4dcd01022487
Ajay <dev@ajay.app>
2025-04-21 20:14:43 -0400

    Fix revanced ua

--- src/utils/userAgent.ts
+++ src/utils/userAgent.ts
@@ -1,13 +1,13 @@
 export function parseUserAgent(userAgent: string): string {
     const ua = userAgent;
 
-    if (ua.match(/(com.google.android.youtube\/)|(com.vanced.android.youtube\/)|(^YouTube\/)|(^Dalvik\/)/i)) {
-        return `Vanced/${ua.match(/.android.youtube\/([^\s]+)/i)[1]}`;
-    }
-
     const revanced = ua.match(/RVX\S+|ReVanced\S+/i);
     if (revanced) {
         return revanced[0];
+    }
+
+    if (ua.match(/(com.google.android.youtube\/)|(com.vanced.android.youtube\/)|(^YouTube\/)|(^Dalvik\/)/i)) {
+        return `Vanced/${ua.match(/.android.youtube\/([^\s]+)/i)[1]}`;
     }
 
     if (ua.match(/(mpv_sponsorblock)|(^python-requests)|(^GuzzleHttp\/)|(^PostmanRuntime\/)/i)) {
