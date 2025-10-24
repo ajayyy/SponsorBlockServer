7d86198a84268c1974dc6c6309358ee9d1561031
Tristan <tristan@tnet.moe>
2025-10-24 09:48:00 +0200

    feat: create database repositories

---
+++ src/databases/repositories/index.ts
@@ -0,0 +1,5 @@
+export * from './sponsorTimesRepository';
+export * from './thumbnailRepository';
+export * from './titleRepository';
+export * from './userRepository';
+export * from './vipRepository';
